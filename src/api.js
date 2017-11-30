2a595c694091ad2add6c9f5714b72c698df88555
gogumai <gonzalo.gumai@gmail.com>
2017-11-30 16:51:50 -0300

    Fixing Lint issues. Corrected some typos.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const request = require('request');
-const Readable = require('stream').Readable;
+const { Readable } = require('stream');
 const ProgressBar = require('ascii-progress');
+
 const API_URL = 'https://api.github.com/gists';
 
 /**
@@ -22,7 +23,6 @@ const postData = (data, callback) => {
   const stringifiedData = JSON.stringify(data);
   const size = Buffer.byteLength(stringifiedData);
 
-  // ------- ProgressBar stuff -------
   let bytesWritten = 0;
   let progress = 0;
   const ProgressBarInterval = setInterval(() => {
@@ -31,39 +31,37 @@ const postData = (data, callback) => {
       clearInterval(ProgressBarInterval);
     }
   }, 100);
-  // ----- End ProgressBar stuff -----
 
-  var postOptions = {
+  const postOptions = {
     url: API_URL,
     json: true,
     headers: {
       'content-type': 'application/json',
       'User-Agent': 'gogumai',
-    }
+    },
   };
 
-  const stream = new Readable;
+  const stream = new Readable();
   stream.push(stringifiedData); // the string to be on the stream
   stream.push(null); // indicates end-of-file - the end of the stream
 
-  const httpRequest = stream.pipe(request.post(postOptions, (err, res, body) => {
+  stream.pipe(request.post(postOptions, (err, res, body) => {
     if (err) {
       progressBar.completed = true;
       progressBar.clear();
-      return callback(err)
+      return callback(err);
     }
     // res.statusCode = 500;
     if (res && (res.statusCode === 200 || res.statusCode === 201)) {
-      return callback(null, body.html_url)
-    } else {
-      return callback(new Error("Couldn't upload file to GitHub"))
+      return callback(null, body.html_url);
     }
+    return callback(new Error("Couldn't upload file to GitHub"));
   })).on('data', (chunk) => {
-      bytesWritten += chunk;
-      progress = (bytesWritten.length * 100) / size;
+    bytesWritten += chunk;
+    progress = (bytesWritten.length * 100) / size;
   });
-}
+};
 
 module.exports = {
   postData,
-}
+};
--- uploader.js
+++ uploader.js
@@ -1,12 +1,11 @@
 const async = require('async');
 const prompt = require('prompt');
 const Utils = require('./src/utils');
-const Api = require('./src/api');
 
 const PATH_ARGV_INDEX = 2;
 const DESCRIPTION_ARGV_INDEX = 3;
-const path = process.argv[PATH_ARGV_INDEX];
-const description = process.argv[DESCRIPTION_ARGV_INDEX];
+const pathArg = process.argv[PATH_ARGV_INDEX];
+const descriptionArg = process.argv[DESCRIPTION_ARGV_INDEX];
 
 /**
  * Main module that gets executed
@@ -17,23 +16,23 @@ const description = process.argv[DESCRIPTION_ARGV_INDEX];
 prompt.start();
 
 // Prompt schema
-var schema = {
+const schema = {
   properties: {
     retry: {
       description: 'retry? (y/n) ',
       default: 'y',
       pattern: /^(?:y|n)$/,
       message: "'You must enter 'y' or 'n'",
-      required: true
+      required: true,
     },
-  }
+  },
 };
 
 /**
-* This function triggers the main execution path way. That beign the asyn's waterfall method
+* This function triggers the main execution path way. That being the async's waterfall method
 * that chains the steps required to perform the upload.
 * It also provides a mechanism to retry the upload if an error ocurs.
-* When the process finishes it logs the gist url.
+* When the process finishes it logs the gist's url.
 * @param {string} path the path given by the user
 * @param {string} description description of the gist
 */
@@ -44,14 +43,14 @@ const main = (path, description) => {
       Utils.resolvePath,
       Utils.createFilesArray,
       Utils.createPostBody,
-      Utils.createPostData
+      Utils.createPostData,
     ],
     (err, result) => {
-      if(err) {
+      if (err) {
         console.log('Something wrong happened: ', err.message);
         // Ask the user for retry
-        prompt.get(schema, function (err, result) {
-          if (result && result.retry === 'y') {
+        prompt.get(schema, (error, userInput) => {
+          if (userInput && userInput.retry === 'y') {
             console.log('Retrying...');
             main(path, description);
           }
@@ -59,7 +58,7 @@ const main = (path, description) => {
         return;
       }
       console.log('Gist url: ', result);
-    }
+    },
   );
-}
-main(path, description);
+};
+main(pathArg, descriptionArg);
